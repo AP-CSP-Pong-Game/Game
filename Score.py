6834accc5e778c7dfbeba560f8ec20fea787fb4e
s51491 <160766066+s51491@users.noreply.github.com>
2024-02-22 11:01:41 -0600

    Working Scorboard

--- Score.py
+++ Score.py
@@ -1,1 +1,20 @@
-print("hello")
+import turtle as trtl 
+
+playerscore = 0
+trtl.goto(0,0)
+
+trtl.forward(1005)
+
+x = trtl.xcor()
+y = trtl.ycor()
+if x >= 1000:
+    playerscore = playerscore + 1
+    print("Score:", playerscore)
+
+if x <= -1000:
+    print("Game Over")
+    print("Your Score is", playerscore)
+
+
+
+trtl.done()
