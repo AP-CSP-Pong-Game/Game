4a87d36574a75084eba8640ba1d89f50cc179000
McCadeM <160766140+McCadeM@users.noreply.github.com>
2024-02-22 11:01:48 -0600

    learning how to code leaderboard

---
+++ leaderboard_code_test.py
@@ -0,0 +1,4 @@
+import turtle as trtl
+import leaderboard_pong as lbp
+def coder(lbp):
+    global score += 50
--- leaderboard_pong.py
+++ leaderboard_pong.py
@@ -1,1 +1,9 @@
-print("hey hello")
+import turtle as trtl
+import leaderboard_code_test as lb
+
+global score
+
+lb.coder()
+print(score)
+
+trtl.done()
