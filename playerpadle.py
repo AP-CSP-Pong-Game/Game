203c90d43702168bc44dba4a993c24384ae0bbdf
LaurenSmithh <160766105+LaurenSmithh@users.noreply.github.com>
2024-02-26 11:02:24 -0600

    i did timer

--- playerpadle.py
+++ playerpadle.py
@@ -1,7 +1,8 @@
 #set up
+import time
 
 import turtle as trtl
-paddle = trtl
+paddle = trtl.Turtle()
 
 screen = trtl.Screen()
 screen.setup(800,600)
@@ -19,40 +20,55 @@ paddle.color(Pcolor)
 paddle.shapesize(3)
 paddle.fillcolor(Pcolor)
 
+speed = 1
+
 startx = 400
 starty = -260
 paddle.goto(startx,starty)
 
+#-----countdown variables-----
+font_setup = ("Arial", 20, "normal")
+
+timer_up = False
+
 #-----countdown writer-----
-counter =  trtl.Turtle()
+counter = trtl.Turtle()
 counter.hideturtle()
 counter.penup()
-counter.goto(0, 300)
+counter.goto(0,0)
+start = time.time()
+
 #-----game functions-----
-import turtle as turtl
-def countdown():
-  global timer, timer_up
-  counter.clear()
-  if timer <= 0:
-    counter.write("Time's Up", font=font_setup)
-    timer_up = True
-  else:
-    counter.write("Timer: " + str(timer), font=font_setup)
-    timer -= 1
-    counter.getscreen().ontimer(countdown, counter_interval) 
 
 #movement for player
 def moveU():
-    paddle.forward(10)
+    paddle.forward(5)
     
 def moveD():
-    paddle.back(10)
+    paddle.back(5)
     
-
+#speed change
+def speedchange():
+  global speed
+  if (time.time() - start) % 5 == 0:
+     speed +=1
+     paddle.speed(speed)
+     print("speed change")
 
 screen.onkeypress(moveU, "Up")
 screen.onkeypress(moveD, "Down")
 screen.listen()
 
+wn = trtl.Screen()
 
-screen.mainloop()
+wn.tracer(0)
+
+#this is gnna get replaced with computer score
+while True:
+  counter.clear()
+  counter.write(int(time.time() - start), font = ("arial", 30))
+  speedchange()
+  wn.update()
+
+
+
